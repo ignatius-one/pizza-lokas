8ca7bb748ce6a2d6904fc64fb0b537359dbe949b
ignatius-one <josegnacio118@gmail.com>
2021-08-16 18:47:58 -0500

    p

---
+++ js/scrip.js
@@ -0,0 +1,5 @@
+document.getElementById('changes-icon').addEventListener('click', () => {
+    document.getElementById('menu-r').classList.toggle('show-menu')
+    document.getElementById('changes-icon').classList.toggle('bar-x')
+
+})
