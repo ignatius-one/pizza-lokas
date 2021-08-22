bdb131a99d510b3439461dbfb8b90fdcf734592b
ignatius-one <josegnacio118@gmail.com>
2021-08-21 22:02:49 -0500

    e

--- js/scrip.js
+++ js/scrip.js
@@ -1,5 +1,17 @@
 document.getElementById('changes-icon').addEventListener('click', () => {
     document.getElementById('menu-r').classList.toggle('show-menu')
     document.getElementById('changes-icon').classList.toggle('bar-x')
+})
 
+let position = window.pageYOffset;
+let nav = document.getElementById('nav');
+
+window.addEventListener('scroll', function () {
+    adress = this.window.pageYOffset
+    if (position >= adress) {
+        nav.style.top = '0'
+    } else {
+        nav.style.top = '-5em'
+    }
+    position = adress
 })
